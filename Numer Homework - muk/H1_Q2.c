96aed008ef32d2a89a47abdfeba0b18b0c93d82d
muk <thitaree.mu2006@gmail.com>
2025-08-21 13:23:47 +0700

    Update H1_Q2.c

--- Numer Homework - muk/H1_Q2.c
+++ Numer Homework - muk/H1_Q2.c
@@ -2,8 +2,10 @@
 #include<math.h>
 
 int main(){
-	double xL =1.5 ,xR =2.0 ,xM ,xMOld =0 ,fxR ,fxM ,error;
+	double xL =1.5 ,xR =2.0 ,xM ,xMOld,fxR ,fxM ,error,i=-1;
 	do {
+		i++;
+		
 		xM = (xL + xR) / 2;
 		fxR = pow(xR,4) - 13;
 		fxM = pow(xM,4) - 13;
@@ -12,10 +14,11 @@ int main(){
 		} else {
 			xR = xM;
 		}
-		error = fabs( (xM - xMOld) / xM);
-		xMOld = xM;
 		
-	} while (error > 0.000001);
+		if (i>0){
+			error = fabs( (xM - xMOld) / xM);
+		}
+		xMOld = xM;
+	} while (i==0 || error > 0.000001);
 	printf("%.6f",xM);
 }
-
